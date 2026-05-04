79f35919b696f4bd8a8dffd818e0bb2793e16fce
biotender <155460658+junior1p@users.noreply.github.com>
2026-05-04 21:15:33 +0800

    Fix BioTender basePath URL handling

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://junior1p.github.io/BioTender/sitemap.xml',
+    sitemap: 'https://www.biotender.online/BioTender/sitemap.xml',
   };
 }
